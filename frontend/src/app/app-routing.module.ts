ebbeae3ae4493baa29212d25cc287e45c0e79074
sYnergieee <maksim228775@gmail.com>
2023-12-08 23:16:18 +0300

    add main page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MapPageComponent } from './map-page/map-page.component';
 import { ModelPageComponent } from './model-page/model-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'map',
+    redirectTo: 'main',
     pathMatch: 'full',
   },
   { path: 'map', component: MapPageComponent },
   { path: 'model', component: ModelPageComponent },
+  { path: 'main', component: MainPageComponent}
 ];
 
 @NgModule({
--- frontend/src/app/shared/consts.ts
+++ frontend/src/app/shared/consts.ts
@@ -1,4 +1,5 @@
 export let LINKS = [
+    { title: 'Главная', link: 'main', icon: 'map' },
     { title: 'Карта', link: 'map', icon: 'ev_station' },
     { title: 'Модель', link: 'model', icon: 'settings' },
 ];
